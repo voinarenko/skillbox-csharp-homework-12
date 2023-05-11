79bf532d9f1f070aaea7da9133d37f1c05e5f368
Sergei Voinarenko <voynarenko@gmail.com>
2023-05-11 19:31:05 +0300

    some changes made.

---
+++ Model/Model.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Homework12.Model;
+
+public class Client
+{
+    public string? Name { get; set; }
+    public List<Account>? Accounts { get; set; }
+}
+
+public abstract class Account
+{
+    public string? AccountNumber { get; set; }
+    public decimal Balance { get; set; }
+}
+
+public class DepositAccount : Account
+{
+    // Additional properties specific to deposit account
+}
+
+public class RegularAccount : Account
+{
+    // Additional properties specific to regular account
+}
--- Model/ValueDescription.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Homework12.Model
-{
-    public class ValueDescription
-    {
-        public object? Value {get; set; }
-        public object? Description {get; set; }
-    }}
